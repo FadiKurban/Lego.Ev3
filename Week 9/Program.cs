e1394862cd7b167bbbd4911f97a2685750843f02
FADI KURBAN <101119897@student.swin.edu.au>
2017-11-03 15:54:04 +1100

    Add project files.

---
+++ Week 9/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+using System.Threading;
+
+namespace Week_9
+{
+    class Program
+    {
+        public static Brick Brick { get; set; }
+        static void Main(string[] args)
+        {
+
+            ConnectToBrick();
+            // Turn180();
+            //  System.Threading.Thread.Sleep(1000);
+            // DriveBackwards();
+            // System.Threading.Thread.Sleep(1000);
+            // Turn180();
+            // DriveForwardwards();
+            Color();
+            Console.Write("Sequence Complete");
+            Console.ReadLine();
+
+        }
+
+        static async void ConnectToBrick()
+        {
+            Brick = new Brick(new UsbCommunication());
+
+            await Brick.ConnectAsync();
+        }
+
+        static async void Turn90()
+        {
+            await Brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.D, 50, 750, false);
+        }
+        static async void Turn180()
+        {
+
+            Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, 100, 400, false);
+            Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, -100, 400, false);
+
+            await Brick.BatchCommand.SendCommandAsync();
+        }
+
+        static async void DriveBackwards()
+        {
+            Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, -100, 1000, false);
+            Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, -100, 1000, false);
+
+            await Brick.BatchCommand.SendCommandAsync();
+        }
+        static async void DriveForwardwards()
+        {
+            Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.A, 100, 1000, false);
+            Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.D, 100, 1000, false);
+
+            await Brick.BatchCommand.SendCommandAsync();
+        }
+
+        static void Colour(object sender, BrickChangedEventArgs e)
+            {
+            Brick.Ports[InputPort.Two].SetMode(ColorMode.Reflective);
+            Console.WriteLine(e.Ports[InputPort.Two].SIValue.ToString());
+        }
+    }
+}
---
+++ WorkingWithSensors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace WorkingWithSensors
+{
+    public class Program
+    {
+        public static Brick brick { get; set; }
+
+        static void Main(string[] args)
+        {
+            ConnectToBrick();
+
+
+            brick.BrickChanged += ButtonChanged;
+            brick.BrickChanged += DistanceChanged;
+
+            Console.WriteLine(brick.Ports[InputPort.One].SIValue.ToString());
+
+            Console.ReadLine();
+        }
+
+        static async void ConnectToBrick()
+        {
+            brick = new Brick(new UsbCommunication());
+            await brick.ConnectAsync();
+        }
+
+        static void ButtonChanged(object sender, BrickChangedEventArgs e)
+        {
+            Console.WriteLine(e.Ports[InputPort.One].SIValue.ToString());
+        }
+
+        static  void DistanceChanged(object sender, BrickChangedEventArgs e)
+        {
+            brick.Ports[InputPort.Two].SetMode(ColorMode.Reflective);
+            Console.WriteLine(e.Ports[InputPort.Two].SIValue.ToString());
+        }
+    }
+}
